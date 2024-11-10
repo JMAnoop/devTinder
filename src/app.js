f4c1935ec34f63c67c9e4ca6670b92ca954ecdfa
Anoop JM <anoop.madhusoodhanan@nyulangone.org>
2024-11-10 18:35:08 +0000

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,25 +1,42 @@
 const express = require('express')
 
 const app = express();
+//multiple handlers
 
+app.get('/user',
 
-app.get('/user', (req, res) => {
-    res.send({firsrName: 'Anoop',
-        lastName: 'JM'
-    })
+[(req, res, next) => 
+{
+    next()
+   // res.send('Handling route user 1')
+},
+
+(req, res, next) => {
+    next();
+   // res.send('Handlig route user 2')
+}],
+
+(req, res) => {
+    res.send('Handling route user 3')
 })
 
-app.post('/user', (req, res) => {
-    res.send('Successful saved the data')
-})
+// app.get('/user', (req, res) => {
+//     res.send({firsrName: 'Anoop',
+//         lastName: 'JM'
+//     })
+// })
 
-app.delete('/user', (req, res) => {
-    res.send('Data Deleted successfully')
-})
+// app.post('/user', (req, res) => {
+//     res.send('Successful saved the data')
+// })
 
-app.use('/test', (req, res) => {
-    res.send('Hello world')
-})
+// app.delete('/user', (req, res) => {
+//     res.send('Data Deleted successfully')
+// })
+
+// app.use('/test', (req, res) => {
+//     res.send('Hello world')
+// })
 
 app.listen(3000, () => {
     console.log('Server is successfully running ...')
